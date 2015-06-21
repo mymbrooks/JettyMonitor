8e08090e5e819a2ee250f98d42f7bef5daec7dea
wangsen <wangsen@ruibu.info>
2015-06-21 11:41:48 +0800

    创建 Jetty Monitor 项目

---
+++ Monitor/EnvironmentUtil.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.Win32;
+
+namespace JettyMonitor
+{
+    public static class EnvironmentUtil
+    {
+        /// <summary>
+        /// 打开系统环境变量注册表
+        /// </summary>
+        /// <returns>RegistryKey</returns>
+        private static RegistryKey OpenSysEnvironment()
+        {
+            RegistryKey key = Registry.LocalMachine;
+            key = key.OpenSubKey("SYSTEM", true);//打开 HKEY_LOCAL_MACHINE 下的 SYSTEM 
+            key = key.OpenSubKey("ControlSet001", true);//打开ControlSet001 
+            key = key.OpenSubKey("Control", true);//打开 Control 
+            key = key.OpenSubKey("Session Manager", true);//打开 Session Manager
+            key = key.OpenSubKey("Environment", true);//打开 Environment
+
+            return key;
+        }
+
+        /// <summary>
+        /// 获取系统环境变量
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static string GetSysEnvironmentByName(string name)
+        {
+            string result = string.Empty;
+            try
+            {
+                result = OpenSysEnvironment().GetValue(name).ToString();//读取
+            }
+            catch
+            {
+                return string.Empty;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 设置系统环境变量
+        /// </summary>
+        /// <param name="name">变量名</param>
+        /// <param name="strValue">值</param>
+        public static void SetSysEnvironment(string name, string value)
+        {
+            OpenSysEnvironment().SetValue(name, value, RegistryValueKind.String);
+        }
+
+        /// <summary>
+        /// 检测系统环境变量名称是否存在
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static bool CheckSysEnvironmentNameExist(string name)
+        {
+            if (!string.IsNullOrEmpty(GetSysEnvironmentByName(name)))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 检测系统环境变量值是否存在
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool CheckSysEnvironmentValueExist(string name, string value)
+        {
+            if (CheckSysEnvironmentNameExist(name))
+            {
+                foreach (string v in GetSysEnvironmentByName(name).Split(';'))
+                {
+                    if (v == value)
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 追加指定环境变量的值
+        /// </summary>
+        /// <param name="name">环境变量名</param>
+        /// <param name="value">待追加的值</param>
+        public static void AppendValue(string name, string value)
+        {
+            if (!CheckSysEnvironmentValueExist(name, value))
+            {
+                string values = GetSysEnvironmentByName(name);
+                if (values.Substring(values.Length - 1, 1) == ";")
+                {
+                    SetSysEnvironment(name, values + value);
+                }
+                else
+                {
+                    SetSysEnvironment(name, values + ";" + value);
+                }
+            }
+        }
+    }
+}
---
+++ Monitor/MainWindow.xaml.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JettyMonitor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private FolderBrowserDialog folder;
+        private Regex reg;
+        public MainWindow()
+        {
+            InitializeComponent();
+            folder = new FolderBrowserDialog();
+            reg = new Regex(@"^\d+$");
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            if (EnvironmentUtil.CheckSysEnvironmentNameExist("JAVA_HOME"))
+            {
+                this.txtJavaHome.Text = EnvironmentUtil.GetSysEnvironmentByName("JAVA_HOME");
+            }
+
+            if (EnvironmentUtil.CheckSysEnvironmentNameExist("JETTY_HOME"))
+            {
+                this.txtJettyHome.Text = EnvironmentUtil.GetSysEnvironmentByName("JETTY_HOME");
+            }
+        }
+
+        private void btnJavaBrowser_Click(object sender, RoutedEventArgs e)
+        {
+            folder.ShowDialog();
+            if (!string.IsNullOrWhiteSpace(folder.SelectedPath))
+            {
+                this.txtJavaHome.Text = folder.SelectedPath;
+            }
+        }
+
+        private void btnJettyHomeBrowser_Click(object sender, RoutedEventArgs e)
+        {
+            folder.ShowDialog();
+            if (!string.IsNullOrWhiteSpace(folder.SelectedPath))
+            {
+                this.txtJettyHome.Text = folder.SelectedPath;
+            }
+        }
+
+        private void btnJettyBaseBrowser_Click(object sender, RoutedEventArgs e)
+        {
+            folder.ShowDialog();
+            if (!string.IsNullOrWhiteSpace(folder.SelectedPath))
+            {
+                this.txtJettyBase.Text = folder.SelectedPath;
+            }
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private bool Validate()
+        {
+            if (!File.Exists(this.txtJavaHome.Text))
+            {
+                this.labMessage.Content = "Java Home 不存在，请重新选择！";
+                return false;
+            }
+
+            if (!File.Exists(this.txtJettyHome.Text))
+            {
+                this.labMessage.Content = "Jetty Home 不存在，请重新选择！";
+                return false;
+            }
+
+            if (!File.Exists(this.txtJettyBase.Text))
+            {
+                this.labMessage.Content = "Jetty Base 不存在，请重新选择！";
+                return false;
+            }
+
+            //本地端口不允许为空
+            if (string.IsNullOrWhiteSpace(this.txtLocalPort.Text) || !reg.IsMatch(this.txtLocalPort.Text) || Int32.Parse(this.txtLocalPort.Text) > 65535)
+            {
+                this.labMessage.Content = "端口范围： 0 -- 65535！";
+                return false;
+            }
+
+            //远程端口可为空，即可以不启用Jetty的远程调试
+            if (!string.IsNullOrWhiteSpace(this.txtRemotePort.Text) && (!reg.IsMatch(this.txtRemotePort.Text) || Int32.Parse(this.txtRemotePort.Text) > 65535))
+            {
+                this.labMessage.Content = "端口范围： 0 -- 65535！";
+                return false;
+            }
+
+            return true;
+        }
+
+        private void btnCopyCommand_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnClearCommand_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCopyResult_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnClearResult_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
